9e5d4e039e32b33e14772b99818beac8a396cc7d
Almir Libório Batista Junior <almir_jesus2@hotmail.com>
2024-12-09 11:46:09 -0300

    altera banco

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,18 @@
-
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: 'mssql',
+  dialect: 'postgres',
   port: parseInt(process.env.DB_PORT),
   logging: false,
 });
 
 const connectDB = async () => {
   try {
+    console.log('Attempting to connect to the database...');
     await sequelize.authenticate();
     console.log('Connected to the SQL Server database successfully.');
   } catch (error) {
